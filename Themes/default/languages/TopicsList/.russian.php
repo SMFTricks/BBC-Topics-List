cf74735f29fd3a938f41d6fe82783e5a74fb31dc
Bugo <cheetah@bk.ru>
2023-12-22 11:29:25 +0500

    Add Russian translation

---
+++ Themes/default/languages/TopicsList/.russian.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @package BBC Topics List
+ * @version 1.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2023, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+$txt['TopicsList_title'] = 'Список тем';
+$txt['TopicsList_description'] = 'Настройте параметры списка тем, доступные моду BBC Topics List.';
+
+$txt['TopicsList_topic_limit'] = 'Установить лимит тем';
+$txt['TopicsList_topic_limit_desc'] = 'Максимальное количество тем для отображения. 0 — отключить лимит.';
+$txt['TopicsList_topic_notags'] = 'Исключить теги при отображении тем';
+$txt['TopicsList_topic_notags_desc'] = 'Например: вместо «[Обучение] Как сварить яйцо» отобразится «Как сварить яйцо»';
+$txt['TopicsList_topic_only'] = 'Парсить BBC Topics List только внутри тем.';
+$txt['TopicsList_topic_only_desc'] = 'Это предотвратит появление списков на других страницах или в действиях (экшенах).';
+
+$txt['TopicsList_use'] = 'Кому разрешено использовать BBC Topics List';
+$txt['permissionname_TopicsList_use'] = 'Использование Topics List BBC';
+$txt['permissionhelp_TopicsList_use'] = 'Список тем не будет парситься в сообщении, если пользователю не разрешено его использовать.';
+$txt['permissionhelp_groups_TopicsList_use'] = $txt['permissionhelp_TopicsList_use'];
+$txt['cannot_TopicsList_use'] = 'Вам не разрешено вставлять список тем.';
+
+$txt['TopicsList_insert_desc'] = 'Вставить список тем из выбранного раздела';
+$txt['TopicsList_insert_title'] = 'Заголовок списка';
+$txt['TopicsList_insert_board'] = 'ID раздела';
+$txt['TopicsList_insert_board_desc'] = 'Используйте 0, чтобы включить все разделы.';
+$txt['TopicsList_insert_alphanumeric'] = 'Только буквенно-цифровые символы';
+$txt['TopicsList_insert_include'] = 'Только указанные символы';
+$txt['TopicsList_insert_include_desc'] = 'Разделять запятой, без пробелов';
+$txt['TopicsList_insert_include_placeholder'] = 'a,b,c,d,y,x,z';
+
+$txt['TopicsList_no_data'] = 'В этом списке нет тем.';
+$txt['TopcisList_disabled'] = 'Topcis Lists сейчас отключен.';
+$txt['TopicsList_default_text'] = 'Список тем';
